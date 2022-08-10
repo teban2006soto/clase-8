1a4bafc7a866b5d40c5516f9bdf111528f9ea43d
teban2006soto <105441640+teban2006soto@users.noreply.github.com>
2022-08-10 16:24:57 -0500

    Add files via upload

---
+++ Esteban-Media/script.js
@@ -0,0 +1,15 @@
+let boton =document.querySelector('boutton')
+let parrafo=document.querySelector('p')
+let contador=0
+
+boton.addEventListener('submit', ()=>{
+alert('su informacion ha sido invalida')
+contador=+1
+contador
+})
+
+let boton =document.querySelector('form')
+boton2.addEventListener('reset', ()=>{
+    alert('estas seguro')
+})
+
